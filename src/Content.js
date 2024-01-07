4a75fbad7ad58d86db589a79bae68cc2ce71acb2
Faisallm <faisallawan1997@gmail.com>
2024-01-07 13:13:34 -0800

    Done with List and Keys

--- src/Content.js
+++ src/Content.js
@@ -1,60 +1,107 @@
 import React from "react";
-import { useState } from 'react';
+import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Content = () => {
   // a state is like a random access memory for a component.
   // react re-renders the items of the component when the state changes.
 
-  // name is the getter.
-  // setName is the setter.
-  // default state name of Faisal   
-  const [name, setName] = useState('Faisal');
-  // the default state is set to zero. 
-  const [count, setCount] = useState(0);
+  // items is the getter.
+  // setItems is the setter.
+  // list of objects
+  // array of objects
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "One half pound bag of Cocoa Covered Almonds Unsalted",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3",
+    },
+  ]);
 
-  // arrow functions
-  // this function returns a random name out of the
-  // names array.
+  // this function will play with the state.
+  const handleCheck = (id) => {
+    // changing the state
+    // we shouldn't change the array directly
+    // we should create a new array.
+    // changing each object at a time.
 
-  const handleNameChange = () => {
-    const names = ["Faisal", "Amina", "Small Faisal", "Small Amina"];
-    // generate a random number between 0-3, 4 excluded
-    const int = Math.floor(Math.random() * 4);
-    // setting the state of the component.
-    // this will cause the component to...
-    // re-render with the new state.
-    setName(names[int]);
+    // changing an object in an array, memorize this pattern!
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    // changing the state
+    setItems(listItems);
+
+    // changing the state permanently using localStorage.
+    // this is similar to redis in python.
+    // it is a fast input-output storage.
+    // setItem
+    // getItem
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
   };
 
+  const handleDelete = (id) => {
 
-  const handleClick = () => {
-    // I suppose to see 1, but instead...
-    // I see 0, weird.
-    setCount(count + 1);
-    setCount(count + 1);
-    // this will not change to 1 from 0,
-    // because 0 was what was brought into...
-    // the function.
-    console.log(count)
-  };
+    // filter out items that don't have the same id as the...
+    // clicked checkbox.
+    // the new array will have id that are not equal to the id...
+    // we passed in.
+    const listItems = items.filter(item => item.id !== id);
+    // changing the state
+    setItems(listItems);
+    // saving the new list to listItems
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
 
-  const handleClick2 = (name) => {
-    console.log(count);
   };
 
   return (
     // the main content of the page.
     // hence the use of the semantic html main.
     <main>
-      <p onDoubleClick={handleClick}>Hello {name}! </p>
-      <button onClick={handleNameChange}>Change Name</button>
-      {/* to pass a parameter to a function during
-      each click event, we need an anonymous function. */}
-      {/* the anonymous function is not called into action
-      until the click. */}
-      <button onClick={handleClick}>Click it</button>
-      {/* passing events to anonymous functions */}
-      <button onClick={handleClick2}>Click event</button>
+      {/* we are going to use unordered items to render
+        the lists. */}
+      {/* this is known as conditional rendering. */}
+      {items.length ? (
+      <ul>
+        {items.map((item) => (
+          // each item of the list should be unique...
+          // hence the use of a key or id.
+          <li className="item" key={item.id}>
+            <input
+              type="checkbox"
+              checked={item.checked}
+              // onChange will change the item.changed value in the state.
+              onChange={() => handleCheck(item.id)}
+            />
+            <label
+              style={item.checked ? { textDecoration: "line-through" } : null}
+              onDoubleClick={() => handleCheck(item.id)}
+            >
+              {item.item}
+            </label>
+            {/* the icon will serve the role
+            of a button */}
+            <FaTrashAlt
+              role="button"
+              tabIndex="0"
+              onClick={() => handleDelete(item.id)}
+            />
+          </li>
+        ))}
+      </ul>
+      ) : (
+        <p style={{ marginTop: '2rem' }}>Your list is empty</p>
+      )}
     </main>
   );
 };
